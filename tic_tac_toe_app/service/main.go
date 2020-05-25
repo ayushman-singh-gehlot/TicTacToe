b9e9ca73422db4fe13566d3dade51796e440f4bc
ayushman singh gehlot <ayushmangehlot8@gmail.com>
2020-05-25 17:28:39 +0530

    updated

--- tic_tac_toe_app/service/main.go
+++ tic_tac_toe_app/service/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 
 	"github.com/ayushman-singh-gehlot/TicTacToe/tic_tac_toe_app/components"
-	"github.com/ayushman-singh-gehlot/TicTacToe/tic_tac_toe_app/service"
 )
 
 func main() {
-	board1 := service.NewBoardService(3)
-	board1.MarkCell(0, components.XMark)
-	fmt.Println(board1.DisplayBoard())
+	fmt.Println(components.Board{})
+	fmt.Println("end of main")
+
+	// board1 := service.NewBoardService(3)
+	// board1.MarkCell(0, components.XMark)
+	// fmt.Println(board1.DisplayBoard())
 
 }
